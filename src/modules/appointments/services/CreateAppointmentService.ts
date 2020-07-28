5e5cd8ed7cc8af77fa848307f7aad6e12d6b6ca2
Ygor <yrmuller@yahoo.com.br>
2020-07-28 10:50:53 -0300

    28072020b

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -3,7 +3,6 @@ import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
 
-//import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository';
 import Appointment from '../infra/typeorm/entities/Appointment';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
@@ -23,8 +22,6 @@ class CreateAppointmentService {
     @inject('NotificationsRepository')
     private notificationsRepository: INotificationsRepository,
 
-   // @inject('CacheProvider')
-   // private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({
@@ -50,7 +47,6 @@ class CreateAppointmentService {
 
     const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
       appointmentDate,
-      //provider_id,
     );
 
     if (findAppointmentInSameDate) {
@@ -69,13 +65,6 @@ class CreateAppointmentService {
       recipient_id: provider_id,
       content: `Novo agendamento para dia ${dateFormatted}`,
     });
-
-    /*await this.cacheProvider.invalidate(
-      `provider-appointments:${provider_id}:${format(
-        appointmentDate,
-        'yyyy-M-d',
-      )}`,
-    );*/
 
     return appointment;
   }
